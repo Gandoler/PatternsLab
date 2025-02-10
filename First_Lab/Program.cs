2e72b7937cd80775e9a0b935234cbf2d1235d7a4
Gandoler <gl.krutoi@mail.ru>
2025-02-10 18:12:18 +0300

    Сделал Первенькое

---
+++ First_Lab/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using First_Lab;
+using Serilog;
+
+
+class Program
+{
+
+    public static void Main()
+    {
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Async(a => a.File("/Users/gl.krutoimail.ru/RiderProjects/PatternsLab/First_Lab/Logs/myapp.log", 
+                rollingInterval: RollingInterval.Month))
+            .CreateLogger();
+
+        Log.Information("App start");
+        First_Lab.ThreadLockSingleton.Instance.HelloWorld(); Log.Information("Hello World in ThreadLockSingleton");
+        First_Lab.SingletonLazy.Instance.HelloWorld(); Log.Information("Hello World in SingletonLazy");
+        First_Lab.SingletonStaticConstr.Instance.HelloWorld(); Log.Information("Hello World in SingletonStaticConstr");
+    }
+}
